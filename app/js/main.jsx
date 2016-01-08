04f7961918abb7e126b103e8f130e67e2471dba5
Kevin Wang <wangkevin448@gmail.com>
2016-01-07 22:54:15 -0800

    partial redux simple router

--- app/js/main.jsx
+++ app/js/main.jsx
@@ -2,13 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router, useRouterHistory} from 'react-router';
 import createHashHistory from 'history/lib/createHashHistory';
+import {createStore, combineReducers} from 'redux';
+import {Provider} from 'react-redux';
+import {syncReduxAndRouter, routeReducer} from 'react-simple-router';
+// import reducers from '<reducers>';
 
 import AppComponent from './components/app/app.jsx';
 
+
+const reducer = combineReducers({/*app reducer file*/});
+const store = createStore(reducer);
+const history = createHashHistory({queryKey: false});
+syncReduxAndRouter(history, store);
+
 ReactDOM.render(
-  <Router history={useRouterHistory(createHashHistory)({queryKey: false})}>
-    {AppComponent.route}
-  </Router>,
+  <Provider store={store}>
+    <Router history={useRouterHistory(history)}>
+      {AppComponent.route}
+    </Router>
+  </Provider>,
   document.getElementById('app'));
 
 /*
@@ -17,6 +29,6 @@ Todo:
 
 - navbar animation on scroll
 - router with redux
-- reduxify the app 
+- reduxify the app
 
 */
